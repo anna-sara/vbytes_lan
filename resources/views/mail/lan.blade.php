95420065a39028087fbc076b06bedb9e3ebfa337
Anna-Sara Sélea <anna-sara@qualitythink.se>
2026-02-25 22:32:23 +0100

    Small fix with email

--- resources/views/mail/lan.blade.php
+++ resources/views/mail/lan.blade.php
@@ -1,8 +1,5 @@
-<x-mail::message>
-# {{ $title }}
-
-{{ $type }} {{ $name }}
-
-{!! str($content)->markdown()->sanitizeHtml() !!}
-
-</x-mail::message>
+<div>
+    <h1>{{$title}}</h1>
+    <p>{{$type}} {{$name}}</p>
+    <div>>{!! str($content)->markdown()->sanitizeHtml() !!}</div>
+<div>
