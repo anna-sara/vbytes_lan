43e4383781ade18f7ffbe98a4ae9cd99bf70c59f
Anna-Sara Sélea <anna-sara@qualitythink.se>
2026-03-01 10:29:20 +0100

    Test with Email/sms template

--- resources/views/mail/lan.blade.php
+++ resources/views/mail/lan.blade.php
@@ -1,4 +1,4 @@
 <div>
     <h1>{{$title}}</h1>
-    <div><span>{{$type}} {{$name}}</span><span>{!! str($content)->markdown()->sanitizeHtml() !!}</span></div>
+    <div><span>{{$type}} {{$name}}</span><span>{!! str($content)->markdown()->stripTags('p')->sanitizeHtml() !!}</span></div>
 <div>
